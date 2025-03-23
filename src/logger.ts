1c254ebee762386e9e2650d127a0225139cc6804
Tim Rogers <me@timrogers.co.uk>
2025-03-23 21:07:54 +0000

    Autocorrect ESLint issues

--- src/logger.ts
+++ src/logger.ts
@@ -4,9 +4,8 @@ const { combine, timestamp, printf, colorize } = winston.format;
 import { Logger } from './types';
 
 // TODO: Figure out how to make ESLint happy with this
-// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+
 const format = printf(({ level, message, timestamp, owner, repo }): string => {
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   if (owner && repo) {
     return `${timestamp} ${level} [${owner}/${repo}]: ${message}`;
   } else {
