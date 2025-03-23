d39a3f24ad795cca1ef0c031f976f35cc44e1fd5
Tim Rogers <me@timrogers.co.uk>
2025-03-23 21:05:41 +0000

    Upgrade ESLint config

---
+++ eslint.config.mjs
@@ -0,0 +1,51 @@
+import { defineConfig } from "eslint/config";
+import prettier from "eslint-plugin-prettier";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("prettier", "eslint:recommended", "plugin:@typescript-eslint/recommended"),
+
+    plugins: {
+        prettier,
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        parserOptions: {
+            project: "./tsconfig.json",
+        },
+    },
+
+    rules: {
+        "prettier/prettier": "error",
+        "@typescript-eslint/strict-boolean-expressions": "off",
+        "@typescript-eslint/no-unnecessary-type-assertion": "off",
+
+        "@typescript-eslint/ban-ts-comment": ["error", {
+            "ts-expect-error": "allow-with-description",
+            minimumDescriptionLength: 3,
+        }],
+    },
+}]);
